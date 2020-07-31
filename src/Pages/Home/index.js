3c118ee18124992c489295f8800c89e0dd393090
Brunno Diego Oliveira Borges <brunno.borges@BOMPMOM010096.local>
2020-07-31 09:58:54 -0300

    tenatnado resolver erro do build

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Menu from './src/components/Menu/index';
 import BannerMain from './src/components/BannerMain/index';
 import Carousel from './src/components/Carousel/index';
-import dadosIniciais from './src/data/dados_iniciais.json';
+import dadosIniciais from '../../data/dados_iniciais.json'
 import Footer from './src/components/Footer/index';
 
 
